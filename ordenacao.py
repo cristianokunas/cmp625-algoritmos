512b48289e48df1f3ce1b33c624c3fc2dcd76ac5
Cristiano Alex Künas <cakunas@inf.ufrgs.br>
2025-06-03 20:30:55 -0300

    refactor: removing unused functions

--- ordenacao.py
+++ ordenacao.py
@@ -20,15 +20,6 @@ def particiona(arr, inicio, fim):
     arr[i + 1], arr[fim] = arr[fim], arr[i + 1]
     return i + 1
 
-# Ordenação por Força Bruta (Bubble Sort)
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    return arr
-
 def quicksort_hibrido(arr, inicio, fim, limiar=20):
     if inicio < fim:
         if (fim - inicio + 1) <= limiar:
